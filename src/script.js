0586209595c5c4acf0158b476435032f564f8abb
singhkunal2050 <singhkunal2050@gmail.com>
2020-10-12 10:30:27 +0530

    working on DOM

--- src/script.js
+++ src/script.js
@@ -2,5 +2,30 @@ document.getElementById('addbtn').addEventListener('click' , addnewTodo )
 
 function addnewTodo(){
   console.log('adding new')
+
+  let todo = document.createElement('div')
+  todo.innerHTML= "this is  mew"
+  let tododetails = document.createElement('p')
+  let todotime = document.createElement('p')
+  let donebtn = document.createElement('button')
+  let delbtn = document.createElement('button')
+
+  document.body.appendChild(todo)
+
 }
+
+document.getElementsByClassName('donebtn')[0].addEventListener('click' , done )
+
+function done(){
+  console.log('done')
+}
+
+document.getElementsByClassName('delbtn')[0].addEventListener('click' , delTodo )
+
+function delTodo(){
+  console.log('deleting')
+}
+
+
+
 
