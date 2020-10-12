f63f9409f5b88f754469b9d2547ece6142fe2ae5
singhkunal2050 <singhkunal2050@gmail.com>
2020-10-12 10:36:21 +0530

    dom updates

--- src/script.js
+++ src/script.js
@@ -4,14 +4,14 @@ function addnewTodo(){
   console.log('adding new')
 
   let todo = document.createElement('div')
-  todo.innerHTML= "this is  mew"
-  let tododetails = document.createElement('p')
-  let todotime = document.createElement('p')
-  let donebtn = document.createElement('button')
-  let delbtn = document.createElement('button')
+  todo.classList.add("todo");
+  todo.innerHTML= ' <p class="tododetails">This is the new todo</p><p class="todotime">05:30 PM</p><button class="donebtn">Completed</button><button class="delbtn">Delete </button>'
+  // let tododetails = document.createElement('p')
+  // let todotime = document.createElement('p')
+  // let donebtn = document.createElement('button')
+  // let delbtn = document.createElement('button')
 
-  document.body.appendChild(todo)
-
+  document.getElementsByClassName('todolist')[0].appendChild(todo)
 }
 
 document.getElementsByClassName('donebtn')[0].addEventListener('click' , done )
